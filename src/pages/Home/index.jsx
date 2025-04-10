f62ade91a6786c7791aac27ed8593d9f39e75932
William Ongaki <ongakiwilliam@gmail.com>
2025-04-10 08:10:55 +0200

    update: modify Home index component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import Hero from './components/Hero';
 import Features from './components/Features';
-import Testimonials from './components/Testimonials';
+import Process from './components/Process';
 import CTA from './components/CTA';
 import './styles/Home.css';
 
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="home">
       <Hero />
       <Features />
-      <Testimonials />
+      <Process />
       <CTA />
     </div>
   );
